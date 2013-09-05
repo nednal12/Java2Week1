53fddfe86cfa1a66c8c3d906c221af7fc93a379f
Brent Marohnic <nednal12@fullsail.edu>
2013-09-05 18:51:50 -0400

    Added XML Layout

--- gen/com/bmarohnic/java2week1/R.java
+++ gen/com/bmarohnic/java2week1/R.java
@@ -24,7 +24,17 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f08000a;
+        public static final int data_address=0x7f080005;
+        public static final int data_city=0x7f080006;
+        public static final int data_name=0x7f080003;
+        public static final int data_source=0x7f080004;
+        public static final int data_state=0x7f080007;
+        public static final int data_title=0x7f080009;
+        public static final int data_zip=0x7f080008;
+        public static final int dealdata=0x7f080002;
+        public static final int searchButton=0x7f080001;
+        public static final int searchField=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -39,15 +49,6 @@ public final class R {
     }
     public static final class style {
         /** 
-        Base application theme, dependent on API level. This theme is replaced
-        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
-    
-
-            Theme customizations available in newer API levels can go in
-            res/values-vXX/styles.xml, while customizations related to
-            backward-compatibility can go here.
-        
-
         Base application theme for API 11+. This theme completely replaces
         AppBaseTheme from res/values/styles.xml on API 11+ devices.
     
@@ -59,10 +60,9 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
-        /**  Application theme. 
- All customizations that are NOT specific to a particular API-level can go here. 
-         */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppBaseTheme=0x7f060003;
+        public static final int AppTheme=0x7f060000;
+        public static final int DealInfo=0x7f060002;
+        public static final int DealLabel=0x7f060001;
     }
 }
