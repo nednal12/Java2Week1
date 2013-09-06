d8cae11f780b297f327d5916aabee6a29f29ed9c
Brent Marohnic <nednal12@fullsail.edu>
2013-09-06 16:41:39 -0400

    Added Service

--- src/com/bmarohnic/java2week1/MainActivity.java
+++ src/com/bmarohnic/java2week1/MainActivity.java
@@ -1,15 +1,50 @@
 package com.bmarohnic.java2week1;
 
+import java.util.Locale;
+
+import com.bmarohnic.java2week1.R;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
+	private EditText editText;
+	private BroadcastReceiver receiver = new BroadcastReceiver() {
+
+	    @Override
+	    public void onReceive(Context context, Intent intent) {
+	      Bundle bundle = intent.getExtras();
+	      if (bundle != null) {
+	        String string = bundle.getString(MyService.FILEPATH);
+	        int resultCode = bundle.getInt(MyService.RESULT);
+	        if (resultCode == RESULT_OK) {
+	          Toast.makeText(MainActivity.this,
+	              "Download complete. Download URI: " + string,
+	              Toast.LENGTH_LONG).show();
+	          editText.setText("Download done");
+	        } else {
+	          Toast.makeText(MainActivity.this, "Download failed",
+	              Toast.LENGTH_LONG).show();
+	          editText.setText("Download failed");
+	        }
+	      }
+	    }
+	  };
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		editText = (EditText) findViewById(R.id.searchField);
 	}
 
 	@Override
@@ -18,5 +53,30 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+	@Override
+	  protected void onResume() {
+	    super.onResume();
+	    registerReceiver(receiver, new IntentFilter(MyService.NOTIFICATION));
+	  }
+	  @Override
+	  protected void onPause() {
+	    super.onPause();
+	    unregisterReceiver(receiver);
+	  }
+	  
+	public void onClick(View view) {
+
+	    Intent intent = new Intent(this, MyService.class);
+	    EditText field = (EditText) findViewById(R.id.searchField);
+		String zipCode = field.getText().toString().toUpperCase(Locale.US);
+	    // Populate values that will be supplied to the service
+//	    intent.putExtra(MyService.FILENAME, "index.html");
+		intent.putExtra(MyService.FILENAME, "JSONData.txt");
+	    intent.putExtra(MyService.URL,
+	        "http://api.8coupons.com/v1/getdeals?key=67714ceb7f857482a7f3e890ae52a8730c7d60663de10661e527d93a9236c547a1c5c3d15f1cb29e6aa3430a54a2091b&zip=" + zipCode + "&categoryid=1&format=json");
+	    startService(intent);
+	    editText.setText("Download Commencing");
+	  }
 
 }
---
+++ src/com/bmarohnic/java2week1/MyService.java
@@ -0,0 +1,85 @@
+package com.bmarohnic.java2week1;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import android.app.Activity;
+import android.app.IntentService;
+import android.content.Intent;
+import android.os.Environment;
+
+public class MyService extends IntentService{
+
+	private int result = Activity.RESULT_CANCELED;
+	public static final String URL = "urlpath";
+	public static final String FILENAME = "filename";
+	public static final String FILEPATH = "filepath";
+	public static final String RESULT = "result";
+	public static final String NOTIFICATION = "com.bmarohnic.android.service.receiver";
+	
+	public MyService() {
+		super("MyService");
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void onHandleIntent(Intent intent) {
+		// TODO Auto-generated method stub
+		
+		String urlPath = intent.getStringExtra(URL);
+	    String fileName = intent.getStringExtra(FILENAME);
+	    File output = new File(Environment.getExternalStorageDirectory(),
+	        fileName);
+	    if (output.exists()) {
+	      output.delete();
+	    }
+
+	    InputStream stream = null;
+	    FileOutputStream fos = null;
+	    try {
+
+	      URL url = new URL(urlPath);
+	      stream = url.openConnection().getInputStream();
+	      InputStreamReader reader = new InputStreamReader(stream);
+	      fos = new FileOutputStream(output.getPath());
+	      int next = -1;
+	      while ((next = reader.read()) != -1) {
+	        fos.write(next);
+	      }
+	      // Finished Successfully
+	      result = Activity.RESULT_OK;
+
+	    } catch (Exception e) {
+	      e.printStackTrace();
+	    } finally {
+	      if (stream != null) {
+	        try {
+	          stream.close();
+	        } catch (IOException e) {
+	          e.printStackTrace();
+	        }
+	      }
+	      if (fos != null) {
+	        try {
+	          fos.close();
+	        } catch (IOException e) {
+	          e.printStackTrace();
+	        }
+	      }
+	    }
+	    publishResults(output.getAbsolutePath(), result);
+		
+	}
+
+	private void publishResults(String outputPath, int result) {
+	    Intent intent = new Intent(NOTIFICATION);
+	    intent.putExtra(FILEPATH, outputPath);
+	    intent.putExtra(RESULT, result);
+	    sendBroadcast(intent);
+	  }
+	
+}
